4a53755ac4e0f3d3ef90f32a1aea83614986c04a
Vinicius Rom <vinem.rox@protonmail.com>
2024-08-29 23:33:48 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("==== TEST 3: seller findAll ===");
+		
+		list = sellerDao.findAll();
+		
+		
+		for (Seller obj:list) {
+			System.out.println(obj);
+		}
+		
 
 	}
 
